c469962fc42949a04f0872004b3ab6ea88d20a47
Pedrin <pedro515vieira@gmail.com>
2022-01-12 14:54:02 -0300

    Todos os Obrigatórios

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Enviar só ativa quando a checkbox estiver marcada
+
 let aceito = document.getElementById('agreement')
 let botao =  document.getElementById('submit-btn')
-
-// if (aceito.checked){
-//     botao.setAttribute('disabled', null)
-// }
 
 aceito.addEventListener('click', function(){
     if (aceito.checked){
         botao.removeAttribute('disabled')
     } else {
         botao.setAttribute('disabled', 'disabled')
+    }
+})
+
+let login = document.getElementById('login-btn')
+let email = document.getElementById('login-input-email')
+let password = document.getElementById('login-input-password')
+
+login.addEventListener('click' ,function(){
+    if (email.value === 'tryber@teste.com' && password.value === '123456'){
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
     }
 })
