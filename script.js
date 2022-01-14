31b68a66cee78541a319326f1dd6b8a6f6e63f9f
Pedrin <pedro515vieira@gmail.com>
2022-01-14 14:51:22 -0300

    acertando o lint

--- script.js
+++ script.js
@@ -1,101 +1,105 @@
 // Enviar só ativa quando a checkbox estiver marcada
 
-let aceito = document.getElementById('agreement')
-let botao =  document.getElementById('submit-btn')
+const aceito = document.getElementById('agreement');
+const botao = document.getElementById('submit-btn');
 
-aceito.addEventListener('click', function(){
-    if (aceito.checked){
-        botao.removeAttribute('disabled')
-    } else {
-        botao.setAttribute('disabled', 'disabled')
-    }
-})
+aceito.addEventListener('click', () => {
+  if (aceito.checked) {
+    botao.removeAttribute('disabled');
+  } else {
+    botao.setAttribute('disabled', 'disabled');
+  }
+});
 
-//Valida o Email na aba de login do header
+// Valida o Email na aba de login do header
 
-let login = document.getElementById('login-btn')
-let email = document.getElementById('login-input-email')
-let password = document.getElementById('login-input-password')
+const login = document.getElementById('login-btn');
+const email = document.getElementById('login-input-email');
+const password = document.getElementById('login-input-password');
 
-login.addEventListener('click' ,function(){
-    if (email.value === 'tryber@teste.com' && password.value === '123456'){
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
-})
+login.addEventListener('click', () => {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
 
-//Contador
+// Contador
 
-let obsEvaluation = document.getElementById("textarea")
-let contador = document.getElementById('counter')
-let caracteres = 500
+const obsEvaluation = document.getElementById('textarea');
+const contador = document.getElementById('counter');
+const caracteres = 500;
 
-obsEvaluation.addEventListener('input', function(event){
-    contador.innerText = (caracteres - obsEvaluation.selectionEnd)
-    console.log(caracteres - obsEvaluation.selectionEnd)
-})
+obsEvaluation.addEventListener('input', () => {
+  contador.innerText = (caracteres - obsEvaluation.selectionEnd);
+  console.log(caracteres - obsEvaluation.selectionEnd);
+});
 
-//Pagina Final
+// Pagina Final
 
-let nameEvaluation = document.getElementById("input-name")
-let lastnameEvaluation = document.getElementById("input-lastname")
-let emailEvaluation = document.getElementById("input-email")
-let houseEvaluation = document.getElementById("house")
-let familyEvaluation = document.getElementsByName('family')
-let subjectsEvaluation = document.getElementsByName('subjects')
-let rateEvaluation = document.getElementsByName("rate")
+const nameEvaluation = document.getElementById('input-name');
+const lastnameEvaluation = document.getElementById('input-lastname');
+const emailEvaluation = document.getElementById('input-email');
+const houseEvaluation = document.getElementById('house');
+const familyEvaluation = document.getElementsByName('family');
+const subjectsEvaluation = document.getElementsByName('subjects');
+const rateEvaluation = document.getElementsByName('rate');
 
-info = {
-    Nome: nameEvaluation.value + " " + lastnameEvaluation.value,
-    Email: emailEvaluation.value,
-    Casa: houseEvaluation.value,
-    Familia: '',
-    Materias: '',
-    Avaliacao: '',
-    Observacoes: obsEvaluation.value,
+const info = {
+  Nome: '',
+  Email: '',
+  Casa: '',
+  Familia: '',
+  Materias: '',
+  Avaliacao: '',
+  Observacoes: '',
 };
 
-botao.addEventListener('click', function(){
-    
-    info.Nome = nameEvaluation.value + " " + lastnameEvaluation.value;
-    info.Email = emailEvaluation.value;
-    info.Casa = houseEvaluation.value;
-    info.Observacoes = obsEvaluation.value;
-    
-    let familia = '';
-    let materia = '';
-    let rate = '';
+function searchFamily() {
+  let familia = '';
 
-    for (i = 0; i < familyEvaluation.length; i++){
-        if(familyEvaluation[i].checked){
-            familia =familyEvaluation[i].value
-            console.log(familia)
-        }
+  for (let i = 0; i < familyEvaluation.length; i + 1) {
+    if (familyEvaluation[i].checked) {
+      familia = familyEvaluation[i].value;
+      console.log(familia);
     }
+  }
 
-    info.familia = familia
-    
-    for (i = 0; i < subjectsEvaluation.length; i++){
-        if(subjectsEvaluation[i].checked){
-            materia = materia + ' ' + subjectsEvaluation[i].value
-            console.log(materia)
-        }
+  info.Familia = familia;
+}
+
+function searchRate() {
+  let rate = '';
+
+  for (let i = 0; i < rateEvaluation.length; i + 1) {
+    if (rateEvaluation[i].checked) {
+      rate = rateEvaluation[i].value;
+      console.log(rate);
     }
+  }
+  
+  info.Avaliacao = rate
+}
 
-    info.Materias = materia
-    
-    for (i = 0; i < rateEvaluation.length; i++){
-        if(rateEvaluation[i].checked){
-            rate = rateEvaluation[i].value
-            console.log(rate)
-        }
+function searchSubjects() {
+  let materia = '';
+
+  for (let i = 0; i < subjectsEvaluation.length; i + 1) {
+    if (subjectsEvaluation[i].checked) {
+      materia = `${materia}
+      ${subjectsEvaluation}`;
+      console.log(materia);
     }
+  }
+  info.Materias = materia;
+}
 
-    info.Avaliacao = rate
+botao.addEventListener('click', searchFamily, searchRate, searchSubjects, () => {
+  info.Nome = `${nameEvaluation.value}  ${lastnameEvaluation.value}`;
+  info.Email = emailEvaluation.value;
+  info.Casa = houseEvaluation.value;
+  info.Observacoes = obsEvaluation.value;
 
-    console.log(info)
-    console.log(info.Avaliacao)
-    console.log(info.Observacoes)
-
-})
+  console.log(info);
+});
